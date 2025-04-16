c7fc7b4ab77a7b3aec6253e9910e591cf6ff5af0
madamgarlic <madam@gentlefarmers.com>
2025-04-16 11:07:12 +0900

    5.0

---
+++ app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="✅ 마늘귀신 Streamlit 앱 테스트", layout="wide")
+st.title("🧄 마늘귀신 자동 패킹리스트 시스템")
+st.subheader("✅ 앱이 정상적으로 실행되고 있습니다!")
+st.write("이 화면이 보이면, Streamlit 배포는 성공입니다.")
